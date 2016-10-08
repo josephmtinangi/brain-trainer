437e6c948ff1389ad53a13be2e0d3ad76a15c47b
Joseph Mtinangi <josephmtinangi@gmail.com>
2016-10-08 11:47:54 +0300

    Display the question

--- app/src/main/java/io/github/josephmtinangi/braintrainer/MainActivity.java
+++ app/src/main/java/io/github/josephmtinangi/braintrainer/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import java.util.Random;
 
@@ -21,12 +22,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         startButton = (Button) findViewById(R.id.startButton);
-
+        TextView sumTextView = (TextView) findViewById(R.id.sumTextView);
 
         Random rand = new Random();
 
         int a = rand.nextInt(21);
         int b = rand.nextInt(21);
+
+        sumTextView.setText(Integer.toString(a) + " + " + Integer.toString(b));
 
     }
 }
