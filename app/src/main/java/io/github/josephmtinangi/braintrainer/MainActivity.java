0b77e6393d7e5a97bc75dfb29400c4129a7294fb
Joseph Mtinangi <josephmtinangi@gmail.com>
2016-10-08 11:52:37 +0300

    Create a list to hold answers and remember the location of the correct answer

--- app/src/main/java/io/github/josephmtinangi/braintrainer/MainActivity.java
+++ app/src/main/java/io/github/josephmtinangi/braintrainer/MainActivity.java
@@ -6,11 +6,14 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     Button startButton;
+    ArrayList<Integer> answers;
+    int locationOfCorrectAnswer;
 
     public void start(View view){
         startButton.setVisibility(View.INVISIBLE);
@@ -30,6 +33,16 @@ public class MainActivity extends AppCompatActivity {
         int b = rand.nextInt(21);
 
         sumTextView.setText(Integer.toString(a) + " + " + Integer.toString(b));
+
+        locationOfCorrectAnswer = rand.nextInt(4);
+
+        for( int i = 0; i < 4; i++){
+            if (i == locationOfCorrectAnswer){
+                answers.add(a + b);
+            }else{
+                answers.add(rand.nextInt(41));
+            }
+        }
 
     }
 }
