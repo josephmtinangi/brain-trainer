c1f36444ded53df245929164273a1f2e477c6424
Joseph Mtinangi <josephmtinangi@gmail.com>
2016-10-08 14:46:30 +0300

    Create options menu

--- app/src/main/java/io/github/josephmtinangi/braintrainer/MainActivity.java
+++ app/src/main/java/io/github/josephmtinangi/braintrainer/MainActivity.java
@@ -3,7 +3,8 @@ package io.github.josephmtinangi.braintrainer;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.GridLayout;
@@ -148,5 +149,13 @@ public class MainActivity extends AppCompatActivity {
 
         gameRelativeLayout = (RelativeLayout) findViewById(R.id.gameRelativeLayout);
 
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.options_menu, menu);
+        return true;
     }
 }
